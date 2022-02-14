b0743523827b2890f188ed73dc9cd64572ec0421
Mario <mario@Marios-MacBook-Air.local>
2022-02-14 19:07:23 +0100

    project initiation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
-function App() {
+import React, { useState, useEffect } from "react";
+import Card from "./components/Card";
+import Search from "./components/Search";
+
+const App = () => {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState("");
+
+  useEffect(() => {
+    const url = `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, [term]);
+
+  const SearchHandler = (text) => {
+    setTerm(text);
+  };
+
   return (
-    <div className="App">
-      Hello World
+    <div className="container mx-auto">
+      <Search searchText={SearchHandler} />
+
+      {!isLoading && images.length === 0 && (
+        <h1 className="text-5xl text-center mx-auto mt-32">No Images Found</h1>
+      )}
+
+      {isLoading ? (
+        <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1>
+      ) : (
+        <div className="grid grid-cols-3 gap-4">
+          {images.map((image) => (
+            <Card key={image.id} image={image} />
+          ))}
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Search.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+const Search = (props) => {
+  const [text, setText] = useState("");
+  const TextHandler = (e) => {
+    setText(e.target.value);
+  };
+
+const onSubmitHandler = (e) => { 
+    e.preventDefault(); 
+
+    props.searchText(text);
+}
+
+  return (
+    <div className="max-w-sm rounded overflow-hidden my-10 mx-auto">
+      <form onSubmit={onSubmitHandler} className="w-full max-w-sm">
+        <div className="flex items-center border-b-2 border-teal-500 py-2">
+          <input
+            onChange={TextHandler}
+            className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+            type="text"
+            placeholder="Search Image Term..."
+          />
+          <button
+            className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounder"
+            type="submit"
+          >
+            Search
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Search;
